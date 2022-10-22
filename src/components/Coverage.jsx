64724a2aa8458f5d53b0dd097a2c22fc69f1c172
lens <okorelens27@gmail.com>
2022-10-22 23:33:34 +0300

    Changing the Coverage Section

--- src/components/Coverage.jsx
+++ src/components/Coverage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Router, Routes } from "react-router-dom";
 import CoverageCard from "../cards/CoverageCard";
 
 const Coverage = () => {
@@ -11,19 +12,21 @@ const Coverage = () => {
         Your House and Ready on the Go.
         <br /> Join Our Community Today
       </p>
-      <div className="cards">
-        <CoverageCard />
-        <CoverageCard />
-        <CoverageCard />
-        <CoverageCard />
-        <CoverageCard />
-        <CoverageCard />
-        <CoverageCard />
-        <CoverageCard />
-        <CoverageCard />
-        <CoverageCard />
-        <CoverageCard />
+      <div className="buttons">
+        <Routes></Routes>
+        <button>Sports</button>
+        <button>Kenyan</button>
+        <button>Music</button>
+        <button>Documentary</button>
+        <button>PPV Events</button>
+        <button>African</button>
+        <button>Kids</button>
+        <button>Religious</button>
+        <button>Indian</button>
+        <button>Entertainment</button>
+        <button>International News</button>
       </div>
+      <div className="cards">{/* <CoverageCard /> */}</div>
     </div>
   );
 };
